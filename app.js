1730e0a26c572d6588084f8b2b450ddbfcf7ff91
Sovlo <94002236+Sovlo1@users.noreply.github.com>
2022-05-04 01:11:21 +0200

    updated server.js

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-const express = requite("express");
+const express = require("express");
 const app = express();
+const helmet = require("helmet");
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(
+    helmet({
+      crossOriginResourcePolicy: { policy: "cross-origin" },
+    })
+  );
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+})
+
+
+module.exports = app;
