5daef37604668cf76df64692fbb7e7b6383e3101
Sovlo <94002236+Sovlo1@users.noreply.github.com>
2022-06-30 03:27:34 +0200

    minor update

--- routes/commentroute.js
+++ routes/commentroute.js
@@ -4,7 +4,6 @@ const commentControl = require("../controllers/commentctrl");
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
 
-// router.get("/comments", commentControl.viewComments);
 router.post("/new", multer, auth, commentControl.addComment);
 router.delete("/delete", auth, commentControl.deleteComment);
 router.get("/:id", auth, commentControl.viewSingleComment);
